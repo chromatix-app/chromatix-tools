e51fbee2b5923d0fc6b00fbd2afda5f246a0a2a4
Alex <abimpson@gmail.com>
2024-04-27 13:34:52 +0100

    Added functions to log playback status to Plex

--- plexTools.js
+++ plexTools.js
@@ -37,6 +37,9 @@ const endpointConfig = {
   library: {
     getAllLibraries: (base) => `${base}/library/sections`,
   },
+  status: {
+    postPlaybackStatus: (base) => `${base}/:/timeline`,
+  },
 };
 
 // ======================================================================
@@ -81,6 +84,27 @@ const raceToSuccess = (promises, errorMessage) => {
       })();
     });
   });
+};
+
+// A SIMPLE FUNCTION TO GET THE BROWSER NAME
+
+const getBrowserName = () => {
+  const userAgent = navigator.userAgent;
+  const browsers = [
+    { name: 'Microsoft Edge', identifier: 'Edg' },
+    { name: 'Brave', identifier: 'Brave' },
+    { name: 'Opera', identifier: ['Opera', 'OPR'] },
+    { name: 'Chrome', identifier: 'Chrome' },
+    { name: 'Chromium', identifier: 'Chromium' },
+    { name: 'Firefox', identifier: 'Firefox' },
+    { name: 'Safari', identifier: 'Safari' },
+    { name: 'Samsung Internet', identifier: 'SamsungBrowser' },
+    { name: 'Microsoft Internet Explorer', identifier: 'Trident' },
+  ];
+  const browser = browsers.find((b) =>
+    Array.isArray(b.identifier) ? b.identifier.some((id) => userAgent.includes(id)) : userAgent.includes(b.identifier)
+  );
+  return browser ? browser.name : 'Unknown';
 };
 
 // ======================================================================
@@ -409,4 +433,115 @@ export const getAllLibraries = (baseUrl, accessToken) => {
       });
     }
   });
+};
+
+// ======================================================================
+// LOG PLAYBACK STATUS
+// ======================================================================
+
+export const logPlaybackStatus = (
+  baseUrl,
+  accessToken,
+  sessionId,
+  type,
+  ratingKey,
+  trackId,
+  state,
+  currentTime,
+  duration
+) => {
+  return new Promise((resolve, reject) => {
+    try {
+      const endpoint = endpointConfig.status.postPlaybackStatus(baseUrl);
+      const browserName = getBrowserName();
+      const params = {
+        type: type,
+        key: trackId,
+        ratingKey: ratingKey,
+        state: state, // playing, paused, stopped
+        time: currentTime, // time in milliseconds
+        playbackTime: currentTime, // time in milliseconds
+        duration: duration, // duration of the media in milliseconds
+        // Add any other necessary data here
+      };
+      axios
+        .get(endpoint, {
+          params: params,
+          headers: {
+            'Content-Type': 'application/json',
+            'X-Plex-Token': accessToken,
+            'X-Plex-Client-Identifier': clientId,
+            'X-Plex-Session-Identifier': sessionId,
+            'X-Plex-Product': appName,
+            'X-Plex-Device-Name': browserName,
+            'X-Plex-Platform': browserName,
+            'X-Plex-Device-Icon': clientIcon,
+          },
+        })
+        .then((response) => {
+          resolve(response);
+        })
+        .catch((error) => {
+          reject({
+            code: 'logPlaybackStatus.1',
+            message: 'Failed to update playback status',
+            error,
+          });
+        });
+    } catch (error) {
+      reject({
+        code: 'logPlaybackStatus.2',
+        message: 'Failed to update playback status',
+        error,
+      });
+    }
+  });
+};
+
+// The below variation is used on window unload in order to log playback as stopped.
+// The fetch method is used instead of axios, with keepalive set to true.
+// This is because axios does not support keepalive, and fetch with keepalive
+// will allow the request to complete even if the page is closed.
+
+export const logPlaybackQuit = (
+  baseUrl,
+  accessToken,
+  sessionId,
+  type,
+  ratingKey,
+  trackId,
+  state,
+  currentTime,
+  duration
+) => {
+  try {
+    const endpoint = endpointConfig.status.postPlaybackStatus(baseUrl);
+    const params = new URLSearchParams({
+      type: type,
+      key: trackId,
+      ratingKey: ratingKey,
+      state: state, // playing, paused, stopped
+      time: currentTime, // time in milliseconds
+      playbackTime: currentTime, // time in milliseconds
+      duration: duration, // duration of the media in milliseconds
+      // Add any other necessary data here
+    }).toString();
+    const fetchUrl = `${endpoint}?${params}`;
+    fetch(fetchUrl, {
+      method: 'GET',
+      keepalive: true,
+      headers: {
+        'Content-Type': 'application/json',
+        'X-Plex-Token': accessToken,
+        'X-Plex-Client-Identifier': clientId,
+        'X-Plex-Session-Identifier': sessionId,
+        'X-Plex-Product': appName,
+        'X-Plex-Device-Name': getBrowserName(),
+        'X-Plex-Platform': getBrowserName(),
+        'X-Plex-Device-Icon': clientIcon,
+      },
+    });
+  } catch (error) {
+    // do nothing
+  }
 };
